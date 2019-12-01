0a3071b8af9da217afb0d88fbeddffc02785f5e0
Joseph Banks <joseph@josephbanks.me>
2019-12-01 21:44:49 +0000

    [day 1] [part 2] [rust]

--- day-1/rust/src/main.rs
+++ day-1/rust/src/main.rs
@@ -4,13 +4,31 @@ use std::io::Read;
 fn main() {
     let input = get_input().expect("Could not open input file, does it exist?");
 
-    let mut fuel_reqs = 0;
+    let mut part1_fuel = 0;
+    let mut part2_fuel = 0;
 
     for line in input.split_whitespace() {
-        fuel_reqs += calculate_fuel(line.parse::<f32>().unwrap());
+        let fuel_of_module = calculate_fuel(line.parse::<f32>().unwrap());
+
+        part1_fuel += fuel_of_module;
+        part2_fuel += fuel_of_module;
+
+        let mut last_fuel = fuel_of_module;
+
+        loop {
+            let fuel_of_fuel = calculate_fuel(last_fuel as f32);
+
+            if fuel_of_fuel <= 0 {
+                break;
+            }
+
+            part2_fuel += fuel_of_fuel;
+            last_fuel = fuel_of_fuel;
+        }
     }
 
-    println!("Part 1: {}", fuel_reqs);
+    println!("Part 1: {}", part1_fuel);
+    println!("Part 2: {}", part2_fuel);
 }
 
 fn get_input() -> Result<String, std::io::Error> {
