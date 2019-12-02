d8d196469d2abc1fd86767d1fe91b699c88ed658
Joseph Banks <joseph@josephbanks.me>
2019-12-02 13:26:03 +0000

    [day 2] [part 2] [rust]

--- day-2/rust/src/main.rs
+++ day-2/rust/src/main.rs
@@ -3,6 +3,8 @@ use std::io::Read;
 
 mod interpreter;
 
+const TO_FIND: i64 = 19690720;
+
 fn main() {
     let input = get_input().expect("Could not open input, does the file exist?");
 
@@ -22,6 +24,28 @@ fn main() {
     }
 
     println!("Part 1: {}", interpreter.fetch(0));
+
+    'outer: for i in 0..99 {
+        'inner: for j in 0..=99 {
+            let mut codes = input.split_terminator(",")
+                .map(|x| x.trim())
+                .map(|x| x.parse::<i64>().unwrap())
+                .collect::<Vec<i64>>();
+
+            codes[1] = i;
+            codes[2] = j;
+            let mut interpreter = interpreter::Interpreter::new(codes);
+
+            while interpreter.is_running {
+                interpreter.step();
+            }
+
+            if interpreter.fetch(0) == TO_FIND {
+                println!("Part 2: 100 * {} + {} = {}", i, j, 100 * i + j);
+                break 'outer;
+            }
+        }
+    }
 }
 
 fn get_input() -> Result<String, std::io::Error> {
